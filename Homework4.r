939911ceb6d4fa6efd49c9faa3c896d91bfcf60c
Brian Mannakee <bmannakee@gmail.com>
2013-04-09 21:41:47 -0700

    more homework!

--- Homework4.r
+++ Homework4.r
@@ -2,16 +2,17 @@ library(MASS)
 
 # Load the prostate data, center and scale the predictors, 
 # center the response, and subset test and train
-pdata <- read.table('prostate.txt')
-pdata <- cbind(scale(pdata[1:8]),pdata[9],pdata[10])
+pdata <- read.csv('prostate.txt',row.names=1,sep='\t')
+pdata <- cbind(scale(pdata[1:8],T,T),pdata[9],pdata[10])
 ptrain <- subset(pdata,train)[-10]
 ptest <- subset(pdata,!train)[-10]
-
+print(dim(ptest))
 # Run linear regression and capture MSE on the test data
 # as well as the standard error of the model
 prost.lm <- lm(lpsa ~ .,ptrain)
 lm.sigma <- summary(prost.lm)$sigma
-lm.mse <- mean(ptest$lpsa - predict(prost.lm,ptest[1:8]))
+lm.mse <- mean(squared.err(ptest$lpsa,predict(prost.lm,ptest[1:8])))
+print(lm.mse)
 
 # Need a function to fold the data
 
@@ -19,7 +20,7 @@ get_folded_indexes <- function(n,k){
   # returns a list with k lists each 
   # with random indices
   # adapted from github code by mcmtroffaes
-  set.seed(52771)
+  set.seed(52772)
   ixs <- 1:n
   sizes <- c()
   ix_list <- list()
@@ -42,37 +43,83 @@ squared.err <- function(y,yhat){
 }
 
 
-cv_folds <- get_folded_indexes(67,10)
+cv_folds <- get_folded_indexes(67,5)
 #########################
 # Ridge Regression
 #########################
 
+#compare the GCV results to my cross-validation
 prost.ridge <- lm.ridge(lpsa ~ .,data=ptrain, lambda=seq(0,10,.1))
-
-# Something here is exceptionally sensitive
-# to the number of folds. This is perhaps not that surprising.
-# I can't find a fold number that matches the GCV output.
-# Best match is k=3, but I am going with 5
-
+gcv.lambda <- which.min(prost.ridge$GCV)
+gcv.ridge <- lm.ridge(lpsa ~ .,data=ptrain,lambda=gcv.lambda)
+gcv.testyhat <- as.matrix(ptest[1:8]) %*% gcv.ridge$coef + gcv.ridge$ym
+print(mean(squared.err(ptest[9],gcv.testyhat)))
 
 mse <- c()
 lambda=seq(0,10,.1)
-for (i in 1:length(lambda)){
+for (i in lambda){
   err <- c()
-  for (j in length(cv_folds)){
+  for (j in 1:length(cv_folds)){
     train_folds <- unlist(cv_folds[-j])
     test_fold <- unlist(cv_folds[j])
     cv_ridge <- lm.ridge(lpsa ~ .,data = ptrain, subset=train_folds,lambda=i)
-    y <- ptrain[unlist(cv_folds[j]),9]
-    yhat <- as.matrix(ptrain[test_fold,1:8]) %*% cv_ridge$coef + cv_ridge$ym
+    y <- ptrain[test_fold,9]
+    yhat <- scale(ptrain[test_fold,1:8],center=F,scale=cv_ridge$scales) %*% cv_ridge$coef + cv_ridge$ym
     err <- append(err,squared.err(y,yhat))
   }
   mse <- append(mse,mean(err))
 }
 best_lambda <- lambda[which.min(mse)]
-print(best_lambda)
-prost.ridge.test <- lm.ridge(lpsa ~ .,data=ptest,lambda=best_lambda)
+prost.ridge.test <- lm.ridge(lpsa ~ .,data=ptrain,lambda=best_lambda)
 testy <- ptest$lpsa
-testyhat <- as.matrix(ptest[1:8]) %*% prost.ridge.test$coef + prost.ridge.test$ym
+testyhat <- scale(ptest[1:8],center=T,scale=prost.ridge.test$scales) %*% prost.ridge.test$coef + prost.ridge.test$ym
+ridge.se <- apply(testy-testyhat,2,sd)
 ridge.mse <- mean(squared.err(testy,testyhat))
 print(ridge.mse)
+print(ridge.se)
+
+
+############################################
+# Best Subset Regression - Exhaustive
+############################################
+
+library(leaps)
+prost.exh <- regsubsets(ptrain[1:8],ptrain$lpsa,method='exhaustive')
+exh.coefs <- coef(prost.exh,id=1:8)
+train.exh.models <- function(coefs){
+  # Runs cross validation for each set of coefficients
+  err <- c()
+  for (i in 1:length(cv_folds)){
+    train_folds <- unlist(cv_folds[-i])
+    test_fold <- unlist(cv_folds[i])
+    # make a data frame for lm, need drop=F to make the one variable case
+    # return a frame instead of a list, which is a huge pain
+    train.frame <- cbind(ptrain[train_folds,9,drop=F],ptrain[train_folds,names(coefs[-1]),drop=F])
+    model <- lm(lpsa ~ .,train.frame)
+    yhat <- predict(model,ptrain[test_fold,names(coefs[-1]),drop=F])
+    y <- ptrain[test_fold,9]
+    err <- append(err,squared.err(yhat,y))
+  }
+  mse <- mean(err)
+  return(mse)
+}
+
+# Calculate MSE for cross-validation on training data of all subsets
+exh.mse <- sapply(exh.coefs,train.exh.models)
+exh.preds <- names(exh.coefs[[which.min(exh.mse)]])[-1]
+print(exh.preds)
+
+exh.test.frame <- cbind(ptrain[9],ptrain[,exh.preds,drop=F])
+exh.best.model <- lm(lpsa ~ .,data=exh.test.frame)
+exh.yhat <- predict(exh.best.model,ptest[,exh.preds,drop=F])
+exh.mse2 <- mean(squared.err(exh.yhat,ptest[9]))
+print(exh.mse2)
+
+
+
+
+
+
+
+
+
